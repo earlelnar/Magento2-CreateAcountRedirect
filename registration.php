6424f7688a3c898fe85bb693fff56f4c0c9d7275
unknown <14094984+mazestricks@users.noreply.github.com>
2018-10-20 00:05:08 +0800

    Added some information

--- registration.php
+++ registration.php
@@ -1,7 +1,9 @@
 <?php
 /**
+ *
  * Magento2-CreateAcountRedirect
  * @author MazeStricks 
+ *
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
